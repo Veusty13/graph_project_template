24c50e151e70ba7d164f71662846f427aac00f99
Veusty13 <steve.rassinot@hotmail.com>
2023-01-29 13:46:57 +0100

    add id of the batch when batch copying data to transactions table

--- scripts/postgres/commands.sh
+++ scripts/postgres/commands.sh
@@ -4,8 +4,10 @@ rm -r ./postgres/
 psql -U postgres -c "drop database if exists graph_project"
 psql -U postgres -c "CREATE DATABASE graph_project"
 psql -U postgres -c "grant all privileges on database graph_project to postgres"
+psql -U postgres -d graph_project -c "create sequence transactions_batch_id;"
 psql -U postgres -d graph_project -c "CREATE TABLE transactions (
   id SERIAL PRIMARY KEY,
+  batch_id BIGINT not null default currval('transactions_batch_id'),
   step NUMERIC,
   type VARCHAR(255),
   amount NUMERIC(11,2), 
@@ -18,6 +20,14 @@ psql -U postgres -d graph_project -c "CREATE TABLE transactions (
   isFraud BOOLEAN,
   isFlaggedfraud BOOLEAN
 );" 
+psql -U postgres -d graph_project -c "create function tgf_transactions_batch_id() returns trigger language plpgsql as \$\$
+begin
+    perform nextval('transactions_batch_id');
+    return null;
+end \$\$;
+create trigger tg_transactions_batch_id
+before insert on transactions
+for each statement execute procedure tgf_transactions_batch_id();"
 echo "successfully created project table with the following schema"
 psql -U postgres -d graph_project -c "SELECT 
    table_name, 
