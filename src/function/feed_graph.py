6f73e7a61efbecdd198f392eaeb8b88c1a27c052
Veusty13 <steve.rassinot@hotmail.com>
2023-02-11 18:42:00 +0100

    wrote logic about adding edges and vertices

--- src/function/feed_graph.py
+++ src/function/feed_graph.py
@@ -1,9 +1,11 @@
 from utils.logger import LOG
 from utils.functions.aws import get_message_from_sqs
 from utils.functions.db import execute_read_query, build_db_config_string
-from config.settings import DB_CONFIG_DICT
+from config.settings import DB_CONFIG_DICT, GREMLIN_CONFIG_DICT
+
 from resources.queries.get_last_batch import query as get_last_batch_query
 import psycopg2
+from utils.functions.gremlins import GremlinClient
 
 
 def lambda_handler(event, context) -> None:
@@ -18,4 +20,15 @@ def lambda_handler(event, context) -> None:
             query=query,
             connection=conn,
         )
-        LOG.info(last_batch_response)
+        gremlin_client = GremlinClient(db_config=GREMLIN_CONFIG_DICT)
+        gremlin_client.get_traversal()
+        # gremlin_client.add_vertex(vertex_type="Person", vertex_id=1, name="Steve")
+        # gremlin_client.add_vertex(vertex_type="Person", vertex_id=2, name="Enzo")
+        # gremlin_client.add_vertex(vertex_type="Person", vertex_id=3, name="Jimmy")
+        # gremlin_client.add_vertex(vertex_type="Person", vertex_id=4, name="Marla")
+        # gremlin_client.add_edge(from_id=1, to_id=2, edge_type="friendship", param="0.8")
+        # gremlin_client.add_edge(from_id=1, to_id=2, edge_type="colleagues", param="0.8")
+        # gremlin_client.add_edge(from_id=2, to_id=1, edge_type="friendship", param="0.8")
+        # gremlin_client.add_edge(from_id=2, to_id=3, edge_type="friendship", param="0.8")
+        # gremlin_client.add_edge(from_id=2, to_id=4, edge_type="love", param="0.8")
+        gremlin_client.close_connection()
--- src/utils/functions/gremlins.py
+++ src/utils/functions/gremlins.py
@@ -1,11 +1,16 @@
 from gremlin_python.process.graph_traversal import GraphTraversalSource
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.process.graph_traversal import __
+from gremlin_python.process.traversal import T
 from gremlin_python.process.anonymous_traversal import traversal
+from typing import Optional
+from utils.logger import LOG
 
 
 class GremlinClient:
     def __init__(self, db_config: dict) -> None:
         self.db_config = db_config
+        self.build_gremlin_config_dict()
 
     def build_gremlin_config_dict(self) -> None:
         protocol = self.db_config["protocol"]
@@ -17,14 +22,56 @@ class GremlinClient:
         self.config_dict = {"url": url, "traversal_source": traversal_source}
 
     def get_traversal(self) -> None:
-        connection = DriverRemoteConnection(**self.config_dict)
-        self.g = traversal().withRemote(connection)
-        # reset traversal source
+        self.connection = DriverRemoteConnection(**self.config_dict)
+        self.g: GraphTraversalSource = traversal().withRemote(self.connection)
+
+    def clear_graph(self) -> None:
         self.g.V().drop().iterate()
         self.g.E().drop().iterate()
 
-    def add_egde(self) -> None:
-        pass
+    def close_connection(self) -> None:
+        self.connection.close()
 
-    def add_node(self) -> None:
-        pass
+    def add_edge(
+        self,
+        from_id: int,
+        to_id: int,
+        edge_type: str,
+        param: Optional[str] = None,
+    ):
+        try:
+            existing_edge = (
+                self.g.V()
+                .has(T.id, from_id)
+                .outE(edge_type)
+                .as_("e")
+                .inV()
+                .has(T.id, to_id)
+                .select("e")
+                .next()
+            )
+            LOG.info(
+                f"existing edge with label {edge_type} between {from_id} and {to_id} : {existing_edge}"
+            )
+        except Exception:
+            LOG.info(
+                f"no existing edge with label {edge_type} between {from_id} and {to_id}"
+            )
+            self.g.V().has(T.id, from_id).addE(edge_type).property("param", param).to(
+                __.V().has(T.id, to_id)
+            ).next()
+
+    def add_vertex(
+        self,
+        vertex_type: str,
+        vertex_id: int,
+        name: Optional[str] = None,
+    ):
+        try:
+            existing_vertex = self.g.V().has(T.id, vertex_id).valueMap().next()
+            LOG.info(f"existing vertex with id {vertex_id} : {existing_vertex}")
+        except Exception:
+            LOG.info(f"no existing vertex with id {vertex_id}")
+            self.g.addV(vertex_type).property(T.id, vertex_id).property(
+                "name", name
+            ).next()
